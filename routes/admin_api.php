76c521a9f07147d404cffd484c670eb658612bda
Rouf Indhra <mroufindhradewasambo@mail.ugm.ac.id>
2026-09-11 02:12:44 +0700

    feat(student): create student queries and end point to ui

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -8,4 +9,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum', 'role:dosen,asisten'])->group(function (): void {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
 });
