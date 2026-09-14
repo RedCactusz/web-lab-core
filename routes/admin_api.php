05a934dd17e7ee6dfb6e411e7921fb61c2cb92ec
Rouf Indhra <mroufindhradewasambo@mail.ugm.ac.id>
2026-09-15 01:42:08 +0700

    register student and practicum endpoint for admin side

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
-use App\Http\Controllers\Admin\MahasiswaController;
+use App\Http\Controllers\Admin\MahasiswaController; 
+use App\Http\Controllers\Admin\PraktikumController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,4 +11,6 @@ Route::middleware(['auth:sanctum', 'role:dosen,asisten'])->group(function (): vo
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+    Route::post('/mahasiswa/import', [MahasiswaController::class, 'importCsv']);
+    Route::get('/praktikum', [PraktikumController::class, 'index']);
 });
